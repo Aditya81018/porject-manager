23c56857797f8a9bd7040787307f9ae92b4a45b3
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2024-10-09 13:56:59 +0530

    feat(themes): :sparkles: add light and dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="prose prose-invert dark:prose-neutral max-w-[100vw] h-screen w-screen flex flex-col bg-background">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
